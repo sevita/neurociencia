dd18fc24dc0059cc44ae84dec2f9704dbcd564d5
Sebastian Alberto Vita <svita@ws4.labo7>
2015-10-22 12:57:55 -0300

    tapping casi listo

--- grupo2/tapping.m
+++ grupo2/tapping.m
@@ -1,16 +1,18 @@
 %variables de configuración 
-resolucion=[300 300];      %resolución de la ventana
-ventana=[500 200]; 	       %posición inicial de la pantalla
+resolucion=[1024 780];      %resolución de la ventana
+ventana=[0 0]; 	       %posición inicial de la pantalla
 clrdepth=32;		       %cantidad de bits de los colores
 estimulo=4;                %cantidad de frames que dura el estímulo
-periodo=0.8;   
+frecuencia=0.8;   
 duracionExperimento=8;     %medida en segundos
+intervalo=0.05;			   %tiempo antes y despues del estímulo 
 
 %variables de inicialización
 screenNum=0;               %número de monitor   
 respuesta=[]; 		       %tiempo en el cual fueron tocadas las teclas
 pantalla='blanca';         %dice que pantalla mostrar
 estimulos=[];              %tiempo en el cual aparecieron los estímulos
+filtrado=[];
 
 %mostar pantalla
 HideCursor; 
@@ -33,14 +35,14 @@ while toc < duracionExperimento
 	if pressed
 		pressedCodes=find(firstPress);
 		for i=1:size(pressedCodes, 2)
-			respuesta = [respuesta firstPress(pressedCodes(i))-comienzo]
+			respuesta = [respuesta firstPress(pressedCodes(i))-comienzo];
 		end
 	end
 
 	tiempo = toc;
-	if mod(tiempo,periodo)<=(estimulo-0.1)*frame & strcmp(pantalla,'negra') %me fijo si hay que cambiar la pantalla o no
+	if mod(tiempo,frecuencia)<=(estimulo-0.1)*frame & strcmp(pantalla,'negra') %me fijo si hay que cambiar la pantalla o no
 		Screen('FillRect', wPtr, circle); 
-		Screen('FillOval', wPtr, 255); 
+		Screen('FillRect', wPtr, 255, [0 0 50 50]); 
 		vbl=Screen('Flip', wPtr);
 		estimulos = [estimulos vbl-comienzo];
 		pantalla = 'blanca';
@@ -57,5 +59,17 @@ end
 PsychHID('KbQueueStop'); %dejo de guardar las teclas
 
 PsychHID('KbQueueRelease'); %borro la cola
+
+for i=1:size(estimulos, 2)
+	aux=false;
+	for j=1:size(respuesta, 2)
+		if respuesta(j) >= estimulos(i)-intervalo & respuesta(j) <= estimulos(i)+intervalo & aux==false
+			filtrado = [filtrado respuesta(j)];
+			aux = true;
+		end
+	end	
+end
+%SI NO HAY RESPUESTA AL ESTÍMULO, LO TIRO
+
 Screen('CloseAll');
 ShowCursor; 

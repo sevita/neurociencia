3f1a244373e320ca344b373a1caffa12c002d185
sevita <sebastian_vita@yahoo.com.ar>
2015-10-23 00:33:36 -0300

    falta grafico

--- grupo2/tapping.m
+++ grupo2/tapping.m
@@ -6,6 +6,7 @@ estimulo=4;                %cantidad de frames que dura el estímulo
 frecuencia=0.8;   
 duracionExperimento=8;     %medida en segundos
 intervalo=0.05;			   %tiempo antes y despues del estímulo 
+tamEstimulo=50;            %tamaño del estímulo
 
 %variables de inicialización
 screenNum=0;               %número de monitor   
@@ -17,10 +18,11 @@ filtrado=[];
 %mostar pantalla
 HideCursor; 
 
-[wPtr,rect]=Screen('OpenWindow', screenNum, 0, [ventana(1) ventana(2) (resolucion(1)+ventana(1)) (resolucion(2)+ventana(2))], clrdepth);
+[wPtr,rect]=Screen('OpenWindow', 0, clrdepth);
 
 black=BlackIndex(wPtr);
-circle=BlackIndex(wPtr);
+dibujoEstimuloX=int64((rect(1)-tamEstimulo)/2);
+dibujoEstimuloY=int64((rect(2)-tamEstimulo)/2);
 
 PsychHID('KbQueueCreate'); %creo una cola para obtener las teclas que se presionaron
 
@@ -41,8 +43,8 @@ while toc < duracionExperimento
 
 	tiempo = toc;
 	if mod(tiempo,frecuencia)<=(estimulo-0.1)*frame & strcmp(pantalla,'negra') %me fijo si hay que cambiar la pantalla o no
-		Screen('FillRect', wPtr, circle); 
-		Screen('FillRect', wPtr, 255, [0 0 50 50]); 
+		Screen('FillRect', wPtr, black); 
+		Screen('FillRect', wPtr, [255 255 0], [0 0 50 50]); 
 		vbl=Screen('Flip', wPtr);
 		estimulos = [estimulos vbl-comienzo];
 		pantalla = 'blanca';
@@ -60,16 +62,20 @@ PsychHID('KbQueueStop'); %dejo de guardar las teclas
 
 PsychHID('KbQueueRelease'); %borro la cola
 
-for i=1:size(estimulos, 2)
+Screen('CloseAll');
+ShowCursor;
+
+for e = estimulos %filtro los datos y los resto
 	aux=false;
-	for j=1:size(respuesta, 2)
-		if respuesta(j) >= estimulos(i)-intervalo & respuesta(j) <= estimulos(i)+intervalo & aux==false
-			filtrado = [filtrado respuesta(j)];
+	for r = respuesta
+		if r >= e-intervalo & r <= e+intervalo+estimulo & aux==false
+			filtrado = [filtrado r-e]; %si no hay respuesta a un estímulo, no se tiene en cuenta
 			aux = true;
 		end
 	end	
 end
-%SI NO HAY RESPUESTA AL ESTÍMULO, LO TIRO
 
-Screen('CloseAll');
-ShowCursor; 
+%gráfico
+plot(filtrado,'-',0,'--','r')
+
+ 

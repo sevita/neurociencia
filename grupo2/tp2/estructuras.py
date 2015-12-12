82aeea7380cf3a2d102133baf92abeb2704ab8c4
sevita <sebastian_vita@yahoo.com.ar>
2015-12-12 17:04:21 -0300

    tp hasta ahora

--- grupo2/tp2/estructuras.py
+++ grupo2/tp2/estructuras.py
@@ -1,11 +1,12 @@
 
 
-def crearCancion (clave, nombreCancion, artista, fechaIngreso, letra, fechas, posiciones, bd):
+def crearCancion (clave, nombreCancion, artista, fechaIngreso, letra, fechas, posiciones, bd, genero):
 	cancion = {}
 	cancion['nombre'] = nombreCancion
 	cancion['artista'] = artista
 	cancion['fechaIngreso'] = fechaIngreso
 	cancion['letra'] = letra
+	cancion['genero'] = genero
 	cancion['fechas'] = [fechas]
 	cancion['posiciones'] = [posiciones]
 
--- grupo2/tp2/obtener_videos_nltk.py
+++ grupo2/tp2/obtener_videos_nltk.py
@@ -7,14 +7,22 @@ import nltk
 import urllib3
 from datetime import *
 from patricia import *
+from PyLyrics import *
 from estructuras import *
 from bs4 import BeautifulSoup
+from pyechonest import *
+    
+config.ECHO_NEST_API_KEY="RC8OZFTSKR6SJWGHF"
 
 bd = {}
 cancionesPorAnio = {}
 
 contadorBien = 0
 contadorMal = 0
+contadorMalMetro = 0
+contadorBienMetro = 0
+contadorMalPyLyrics = 0
+contadorBienPyLyrics = 0
 
 def normalizarNombreArtista(nombre):
     borrar = nombre.find(" Featuring ")
@@ -23,7 +31,6 @@ def normalizarNombreArtista(nombre):
     borrar = nombre.find(" featuring ")
     if borrar != -1:
         nombre = nombre[:borrar] #borra todo lo que le sigue a Featuring
-
     
     borrar = nombre.find(" & ")
     if borrar != -1:
@@ -43,10 +50,12 @@ def normalizarNombreArtista(nombre):
     if borrar != -1:
         nombre = nombre[:borrar] 
     
+    nombre = nombre.replace('$', 's')
+
     return normalizarNombre(nombre)
 
 def normalizarNombre(nombre): 
-    caracteresAEliminar = ",!@#$'?/%\\&"
+    caracteresAEliminar = ".,!@#$'?/%\\&"
     for caracter in caracteresAEliminar:
         nombre = nombre.replace(caracter,"")
     
@@ -56,27 +65,42 @@ def normalizarNombre(nombre):
 
     return nombre
 
-def obtenerLetra(nombreCancion, nombreArtista): #falta sacar los [] que aparecen en algunas canciones
+def obtenerLetraPyLyrics(nombreCancion, nombreArtista): #falta sacar los [] que aparecen en algunas canciones
+    global contadorMalPyLyrics
+    global contadorBienPyLyrics
+
+    try:
+        print "PyLyrics"
+        contadorBienPyLyrics = contadorBienPyLyrics + 1
+        return PyLyrics.getLyrics(nombreArtista,nombreCancion) #obtengo la leta de PyLyrics  
+    except:
+        contadorMalPyLyrics = contadorMalPyLyrics + 1
+        return obtenerLetraMetrolyrics(nombreCancion, nombreArtista)
+
+def obtenerLetraMetrolyrics(nombreCancion, nombreArtista): #falta sacar los [] que aparecen en algunas canciones
     cancion = normalizarNombre(nombreCancion)
     artista = normalizarNombreArtista(nombreArtista)
 
     url = 'http://www.metrolyrics.com/' + cancion + '-lyrics-' + artista + '.html'
     global contadorMal
     global contadorBien
+    global contadorMalMetro
+    global contadorBienMetro
 
     try:
+        print "Metrolyrics"
         letra = ''
         html = http.request('GET', url, preload_content=False).read()
         soup = BeautifulSoup(html, 'html.parser')
         soup = soup.find(id="lyrics-body-text") #obtengo la letra
         for verso in soup.findAll('p'):
             letra = letra + verso.get_text() + ' ' #obtengo los versos
-        contadorBien = contadorBien + 1
+        contadorBienMetro = contadorBienMetro + 1
         return letra.replace("\n", " ").rstrip() #saco los enter's y el último espacio en blanco
         
     except:
-        print 'Fallo la URL del Artista: ' + nombreArtista + ' Cancion: ' + nombreCancion + '\n'
-        contadorMal = contadorMal + 1
+        contadorMalMetro = contadorMalMetro + 1
+        return obtenerLetraLyricsfreak(nombreCancion, nombreArtista)
 
 def actualizarLetraMetrolyrics(): 
     nombreCancion = raw_input('Ingrese el nombre de la cancion: ')
@@ -92,7 +116,44 @@ def actualizarLetraMetrolyrics():
     for verso in soup.findAll('p'):
         letra = letra + verso.get_text() + ' ' #obtengo los versos
     bd[clave]['letra'] = letra.replace("\n", " ").rstrip() #saco los enter's y el último espacio en blanco
-   
+
+def obtenerLetraLyricsfreak(nombreCancion, nombreArtista): 
+    artista = normalizarNombreArtista(nombreArtista)
+
+    url = 'http://www.lyricsfreak.com/l/' + artista + '/' #busco el artista
+    global contadorMal
+    global contadorBien
+    global contadorBienMetro
+    global contadorMalMetro
+
+    try:
+        print "Lyricsfreak"
+        html = http.request('GET', url, preload_content=False).read()
+        soup = BeautifulSoup(html, 'html.parser')
+        
+        titulo = nombreCancion + " Lyrics"
+        soup = soup.find(title=titulo) #busco la url de la cancion
+        
+        url = "http://www.lyricsfreak.com/" + soup["href"] 
+        
+        html = http.request('GET', url, preload_content=False).read() #busco la canción
+        soup = BeautifulSoup(html, 'html.parser')
+        soup = soup.find(id='content_h')
+        
+        letra = str(soup) #limpio la letra de la cancion
+        letra = letra.replace('<br><br>', ' ')
+        letra = letra.replace('<br>', ' ')
+        letra = letra.replace('</br>', ' ')
+        letra = letra.replace('</div>', ' ')
+        letra = letra.replace('<div class="dn" id="content_h">', '').rstrip()
+        
+        contadorBien = contadorBien + 1
+        return letra
+        
+    except:
+        print 'Fallo la URL del Artista: ' + nombreArtista + ' Cancion: ' + nombreCancion + '\n'
+        contadorMal = contadorMal + 1
+    
 def obtenerNombresYoutube(url): #devuelve un arreglo con todas los nombres de los videos de un playlist
     html = http.request('GET', url, preload_content=False).read()
     soup = BeautifulSoup(html, 'html.parser')
@@ -113,7 +174,7 @@ def obtenerRankingBillboard(fecha):
     if anio not in cancionesPorAnio: #creo un arreglo en donde se van a guardas las canciones que sonaron un año determinado
         cancionesPorAnio[anio] = []
 
-    print anio
+    print fecha
     for elem in cancionesDelRanking:
         nombreCancion = elem.find("h2").get_text().strip()
         nombreArtista = elem.find("h3").get_text().strip()
@@ -121,7 +182,16 @@ def obtenerRankingBillboard(fecha):
         clave = normalizarNombre(nombreCancion)+normalizarNombreArtista(nombreArtista)
 
         if clave not in bd:
-            crearCancion (clave, nombreCancion, nombreArtista, fecha, obtenerLetra(nombreCancion, nombreArtista), fecha, posicion, bd) 
+            try: #obtengo el genero musical con echonest
+                canciones = song.search(artist='Taylor Swift', title='Blank Space')
+                cancion = canciones[0]
+                genero = cancion.song_type
+            except: 
+                genero = "none"
+
+            print genero
+
+            crearCancion (clave, nombreCancion, nombreArtista, fecha, obtenerLetraPyLyrics(nombreCancion, nombreArtista), fecha, posicion, bd, genero) 
             cancionesPorAnio[anio].append(clave)        
         else:
             bd[clave]['fechas'].append(fecha)
@@ -129,7 +199,7 @@ def obtenerRankingBillboard(fecha):
 
 def descargarRankingBillboard():
     fecha = date(1958,8,9) #primer chart Billboard
-
+    
     while fecha < date.today():
         obtenerRankingBillboard(fecha.strftime("%Y-%m-%d"))
         fecha = fecha + timedelta(days=7)   
@@ -140,16 +210,26 @@ if __name__ == "__main__":
     #print obtenerNombresYoutube('https://www.youtube.com/watch?v=vLv59iiCkZA&list=PLFgquLnL59amyKLSulnqkHkLf-GZRVmtN&index=199')
     
     #print normalizarNombreArtista("Elvis Presley With The Jordanaires")
-    #print obtenerLetra(pepe,'justin bieber')
+    #print obtenerLetraMetrolyrics(pepe,'justin bieber')
 
-    print bd
     #obtenerRankingBillboard('2015-11-21')
-
+    #obtenerLetraLyricsfreak("What Am I Living For", "Chuck Willis") 
     descargarRankingBillboard()
     print bd
+    print cancionesPorAnio
     print len(bd)
-
+    
+   #obtenerLetraLyricsfreak("Feel The Need","Leif Garrett")
    # global contadorMal
    # global contadorBien
-    print contadorBien
-    print contadorMal
+    print "Lyricsfreak bien: " + str(contadorBien)
+    print "Lyricsfreak mal: " + str(contadorMal)
+    print "metrolyrics bien: " + str(contadorBienMetro)
+    print "metrolyrics mal: " + str(contadorMalMetro)
+    print "PyLyrics bien: " + str(contadorBienPyLyrics)
+    print "PyLyrics mal: " + str(contadorMalPyLyrics)
+
+    totalBien = contadorBien + contadorBienMetro + contadorBienPyLyrics
+    print "total bien: " + str(totalBien)
+
+    print "total mal: " + str(contadorMal)
